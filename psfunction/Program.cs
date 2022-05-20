d8cbcfbbdbbe55de506533b20543725cf6609696
cpr <1254517839@qq.com>
2022-05-20 20:40:45 +0800

    增加了导入壁纸和显示壁纸库文件的窗口，注意现在程序默认启动窗口现在是壁纸库

--- psfunction/Program.cs
+++ psfunction/Program.cs
@@ -17,7 +17,7 @@ namespace psfunction
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WPLib());
         }
     }
 }
