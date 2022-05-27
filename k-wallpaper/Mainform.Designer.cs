36ad80fbb5e6e926db10b024ec8570043c9cba34
乔伊凡 <2567044299@qq.com>
2022-05-27 20:34:43 +0800

    壁纸应用到桌面，现在壁纸尺寸有问题

--- k-wallpaper/Mainform.Designer.cs
+++ k-wallpaper/Mainform.Designer.cs
@@ -45,10 +45,12 @@
             this.uiImageButton1.Location = new System.Drawing.Point(106, 233);
             this.uiImageButton1.Name = "uiImageButton1";
             this.uiImageButton1.Size = new System.Drawing.Size(412, 252);
+            this.uiImageButton1.Style = Sunny.UI.UIStyle.Custom;
             this.uiImageButton1.TabIndex = 0;
             this.uiImageButton1.TabStop = false;
             this.uiImageButton1.Text = "本地壁纸";
             this.uiImageButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiImageButton1.Click += new System.EventHandler(this.uiImageButton1_Click);
             // 
             // uiImageButton2
             // 
@@ -59,6 +61,7 @@
             this.uiImageButton2.Location = new System.Drawing.Point(599, 233);
             this.uiImageButton2.Name = "uiImageButton2";
             this.uiImageButton2.Size = new System.Drawing.Size(383, 252);
+            this.uiImageButton2.Style = Sunny.UI.UIStyle.Custom;
             this.uiImageButton2.TabIndex = 1;
             this.uiImageButton2.TabStop = false;
             this.uiImageButton2.Text = "待办事项";
@@ -78,6 +81,7 @@
             this.uiButton1.Text = "使用说明";
             this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiButton1.Click += new System.EventHandler(this.uiButton1_Click);
             // 
             // uiLinkLabel1
             // 
@@ -95,6 +99,7 @@
             this.uiLinkLabel1.Text = "关于我们";
             this.uiLinkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
             this.uiLinkLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiLinkLabel1.Click += new System.EventHandler(this.uiLinkLabel1_Click);
             // 
             // Mainform
             // 
@@ -110,7 +115,7 @@
             this.Name = "Mainform";
             this.RectColor = System.Drawing.SystemColors.ActiveBorder;
             this.Style = Sunny.UI.UIStyle.Custom;
-            this.Text = "Form1";
+            this.Text = "K-wallpaper";
             this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 800, 450);
             ((System.ComponentModel.ISupportInitialize)(this.uiImageButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.uiImageButton2)).EndInit();
