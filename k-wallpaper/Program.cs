7148db036df404d1818069ca2d36ee20a4275762
DESKTOP-OQ8N8ON\DELL <1198494046@qq.com>
2022-05-28 11:04:59 +0800

    美化了psfunction的窗口外观。并且图片入口是全局变量picpath，也可以自己另外打开图片

--- k-wallpaper/Program.cs
+++ k-wallpaper/Program.cs
@@ -16,7 +16,7 @@ namespace k_wallpaper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Mainform());
+            Application.Run(new PSFunctionForm());
         }
     }
 }
